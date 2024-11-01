053ab5e158c342119b5cdce81269e313af332524
Nick1093 <nicholasalexanderlam@gmail.com>
2024-11-01 12:45:04 -0400

    added .env variables

--- server/main.py
+++ server/main.py
@@ -13,6 +13,23 @@ from datetime import datetime, timezone
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+load_dotenv()
+
+# Configure Firebase using environment variables
+firebase_credentials = {
+    "type": os.getenv("TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),  # format private key
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("UNIVERSE_DOMAIN")
+}
+
 cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
@@ -26,10 +43,6 @@ class DreamSummaryInfo(BaseModel):
 class DreamCreate(BaseModel):
     title: str
     summary: str
-
-
-# Load environment variables
-load_dotenv()
 
 # Configure OpenAI
 openai.api_key = os.getenv("OPENAI_API_KEY")
